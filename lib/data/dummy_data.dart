689f55a4a8a8c13052475dc26d74e92bd0ad4ef6
Divyam Divesh <divyamdivesh13@gmail.com>
2025-03-21 20:33:50 +0530

    added models and data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,22 @@
+
+import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/catrgory.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Chicken Breast',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
